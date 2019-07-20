ab4d748771adde06d137faf9019414bae4ce3599
Michal <michal.wrona@fitqbe.com>
2019-07-20 13:25:53 +0200

    first view in twig

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = random_int(0, 100);
+
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
+        return $this->render('lucky/number.html.twig', ['number' => $number]);
+    }
+}
